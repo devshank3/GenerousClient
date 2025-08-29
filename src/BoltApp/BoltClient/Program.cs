27442dac9825ad1a17eac8b91e974adf05c60cc4
Shankar Hariharan K <52159502+devshank3@users.noreply.github.com>
2025-08-29 23:27:42 +0530

    Add BoltClient SignalR client project
    Introduces the BoltClient project to the solution, including configuration, project file, dependencies, and a basic SignalR client implementation that connects to accessorHub and handles view count updates.

---
+++ src/BoltApp/BoltClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace BoltClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7063/accessorHub")
+                .Build();
+
+
+            connection.Closed += async (error) =>
+            {
+                Console.WriteLine("Connection closed. Trying to restart...");
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+            connection.Reconnecting += error =>
+            {
+                Console.WriteLine("Connection lost. Reconnecting...");
+                return Task.CompletedTask;
+            };
+
+            connection.Reconnected += connectionId =>
+            {
+                Console.WriteLine($"Reconnected. ConnectionId: {connectionId}");
+                return Task.CompletedTask;
+            };
+
+            connection.On<int>("UpdateViewCount", (viewCount) =>
+            {
+                Console.WriteLine($"View count: {viewCount}");
+            });
+
+            try
+            {
+                connection.StartAsync().Wait();
+                Console.WriteLine("Connection started successfully.");
+                await connection.InvokeAsync("NotifyWatching");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Connection failed: {ex.Message}");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
